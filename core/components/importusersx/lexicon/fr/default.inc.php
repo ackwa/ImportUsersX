b92ee7692bb68fdf9a1cc32891e91b741fd5631e
Kevin Pause <kevin.pause@supinfo.com>
2012-04-18 14:53:39 +0200

    Ajout du lexicon en et fr

---
+++ core/components/importusersx/lexicon/fr/default.inc.php
@@ -0,0 +1,24 @@
+<?php
+
+$_lang['importusersx'] = 'ImportUsersX';
+$_lang['importusersx.desc'] = 'Ajoute des utilisateurs ModX en utilisant un fichier .csv';
+$_lang['importusersx.intro'] = 'Remplir les champs et cliquer sur Importer . Votre fichier .csv doit être de la forme "prénom;nom;email".';
+$_lang['importusersx.groupName'] = 'Nom du groupe utilisateurs'; 
+$_lang['importusersx.groupName.desc'] = 'Nom du groupe où les utilisateurs seront ajoutés.';
+$_lang['importusersx.noGroupName'] = 'Le nom du groupe utilisateurs est requis.';
+$_lang['importusersx.adminEmailChunk'] = 'Chunk email administrateur';
+$_lang['importusersx.adminEmailChunk.desc'] = 'Nom du chunk pour l\'email qui sera envoyé à l\'administrateur.';
+$_lang['importusersx.noAdminEmailChunk'] = 'Le nom du chunk email administrateur est requis.';
+$_lang['importusersx.userEmailChunk'] = 'Chunk email utilisateur';
+$_lang['importusersx.userEmailChunk.desc'] = 'Nom du chunk pour l\'email qui sera envoyé à l\'utilisateur.';
+$_lang['importusersx.noUserEmailChunk'] = 'Le nom du chunk email utilisateur est requis.';
+$_lang['importusersx.csvFile'] = 'Fichier CSV';
+$_lang['importusersx.csvFile.desc'] = 'Selectionner le fichier .csv qui contient les informations utilisateurs.';
+$_lang['importusersx.noCsvFile'] = 'Le fichier CSV est requis.';
+$_lang['importusersx.adminUsername'] = 'Nom d\'utilisateur de l\'administrateur';
+$_lang['importusersx.adminUsername.desc'] = 'Nom d\utilisateur de l\'administrateur.';
+$_lang['importusersx.noAdminUsername'] = 'Le nom d\'utilisateur de l\'administrateur est requis.';
+$_lang['importusersx.button'] = 'Importer';
+$_lang['importusersx.parsingCsv'] = "Récupération des données à partir du fichier CSV.\n";
+
+?>
