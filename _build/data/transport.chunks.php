0b97ee44bcacc447db6bde86e13ed4c17ed8b170
Tzoreol <kevin.pause@supinfo.com>
2012-04-24 20:34:23 +0200

    Correction erreur d'ajout d'un chunk lors de la build

--- _build/data/transport.chunks.php
+++ _build/data/transport.chunks.php
@@ -16,7 +16,7 @@ $chunks[1]->fromArray(array(
 
 $chunks[2]= $modx->newObject('modChunk');
 $chunks[2]->fromArray(array(
-    'id' => 1,
+    'id' => 2,
     'name' => 'AdminEmailChunk',
     'description' => 'Chunk for the email which will be sent to the administrator.',
     'chunk' => getChunkContent($aSources['chunks'].'adminEmail.chunk.php'),
