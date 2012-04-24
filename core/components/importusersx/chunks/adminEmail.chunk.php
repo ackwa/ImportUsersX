ac431285bc7dba128b3773332beab34be64477a3
Tzoreol <kevin.pause@supinfo.com>
2012-04-24 19:45:46 +0200

    Ajout des chunks

---
+++ core/components/importusersx/chunks/adminEmail.chunk.php
@@ -0,0 +1,7 @@
+<p>Hello,</p>
+
+    [[+addCount:gt=`0`:then=`<p>[[+addCount]] account[[+addCount:gt=`1`:then=`s`:else``]] has been created : </p>
+    	<p>[[+addLog]]</p><br/>]]
+
+	[[+changeCount:gt=`0`:then=`<p>[[+changeCount]] account[[+changeCount:gt=`1`:then=`s`:else``]] has been created : </p>
+    	<p>[[+chhangeLog]]</p><br/>]]
---
+++ core/components/importusersx/chunks/userEmail.chunk.php
@@ -0,0 +1,8 @@
+<p>Hello,</p>
+
+    <p>An account was created for you in [[+sname]]. Here are your login details :</p>
+
+    <p><strong>Username:</strong> [[+username]]<br />
+    <strong>Password:</strong> [[+password]]</p>
+
+    <p>Once you log into [[+sname]] at [[+surl]], you can change your password.</p>
