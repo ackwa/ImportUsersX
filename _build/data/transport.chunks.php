2c8cbbcc379db6d468888a098483d4f7966745ad
Tzoreol <kevin.pause@supinfo.com>
2012-04-25 14:45:29 +0200

    Correction erreur installation chunks

--- _build/data/transport.chunks.php
+++ _build/data/transport.chunks.php
@@ -8,18 +8,16 @@ $chunks = array();
  
 $chunks[1]= $modx->newObject('modChunk');
 $chunks[1]->fromArray(array(
-    'id' => 1,
     'name' => 'UserEmailChunk',
     'description' => 'Chunk for the email which will be sent to the user.',
-    'chunk' => getChunkContent($aSources['chunks'].'userEmail.chunk.php'),
+    'snippet' => getChunkContent($aSources['chunks'].'userEmail.chunk.php'),
 ),'',true,true);
 
 $chunks[2]= $modx->newObject('modChunk');
 $chunks[2]->fromArray(array(
-    'id' => 2,
     'name' => 'AdminEmailChunk',
     'description' => 'Chunk for the email which will be sent to the administrator.',
-    'chunk' => getChunkContent($aSources['chunks'].'adminEmail.chunk.php'),
+    'snippet' => getChunkContent($aSources['chunks'].'adminEmail.chunk.php'),
 ),'',true,true);
  
 return $chunks;
