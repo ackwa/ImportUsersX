08c5a807ef50e20da60071b41767190d9cb7a039
Tzoreol <kevin.pause@supinfo.com>
2012-04-25 16:20:24 +0200

    Correction affichage email admin

--- core/components/importusersx/chunks/adminEmail.chunk.php
+++ core/components/importusersx/chunks/adminEmail.chunk.php
@@ -3,5 +3,5 @@
     [[+addCount:gt=`0`:then=`<p>[[+addCount]] account[[+addCount:gt=`1`:then=`s`:else=``]] has been created : </p>
     	<p>[[+addLog]]</p><br/>`]]
 
-	[[+changeCount:gt=`0`:then=`<p>[[+changeCount]] account[[+changeCount:gt=`1`:then=`s`:else=``]] has been created : </p>
+	[[+changeCount:gt=`0`:then=`<p>[[+changeCount]] account[[+changeCount:gt=`1`:then=`s`:else=``]] has been changed : </p>
     	<p>[[+changeLog]]</p><br/>`]]
