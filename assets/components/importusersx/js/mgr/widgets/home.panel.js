de0dc2bbcfe92ba08f0b12a6580bbe0a70771e6f
Tzoreol <kevin.pause@supinfo.com>
2012-04-26 10:52:21 +0200

    Correction du bug du au copier/coller

--- assets/components/importusersx/js/mgr/widgets/home.panel.js
+++ assets/components/importusersx/js/mgr/widgets/home.panel.js
@@ -21,29 +21,7 @@
  * Place, Suite 330, Boston, MA 02111-1307 USA
  *
  */
- 
-Ext.onReady(function() {
-	
-    MODx.load({ xtype: 'importusersx-page-home'});
-	
-});
- 
-importUsersX.page.Home = function(config) {
-	
-    config = config || {};
-    Ext.applyIf(config,{
-    	components: [{
-        	xtype: 'importusersx-panel-home'
-            ,renderTo: 'importusersx-panel-home-div'
-        }]
-    });
-	
-    importUsersX.page.Home.superclass.constructor.call(this,config);
-	
-};
 
-Ext.extend(importUsersX.page.Home,MODx.Component);
-Ext.reg('importusersx-page-home',importUsersX.page.Home);
 importUsersX.panel.Home = function(config) {
 	
 	config = config || {};
